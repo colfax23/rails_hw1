c48cb52ba9027e385ec15cab0428af071f127ac9
Colfax Selby <cole31_31@yahoo.com>
2013-04-01 17:47:29 -0400

    added app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw1::Application.config.secret_token = '2b5a4c9e7cc08ad8ea04aea8bdfdcab3e8da25bd5234363cacc92dba8ebf5f5c67aad52cf7aebe19780ba0c99054ed7ab09b2477fd9336b597955e211b94ebcd'
